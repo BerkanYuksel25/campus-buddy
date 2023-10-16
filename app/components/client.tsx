f6d2f7a969e0e11d63fc2e970b5087f3935d2972
BerkanYuksel25 <berkan.yuksel25@gmail.com>
2023-10-16 22:43:51 +1100

    CB-5: Adding a back button to the main layout

--- app/components/client.tsx
+++ app/components/client.tsx
@@ -1,8 +1,24 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { usePathname, useRouter } from "next/navigation";
 import { ButtonHTMLAttributes } from "react";
+
+export const BackButton = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const invalidRoutes = ["/", "/sign-in"];
+
+  if (invalidRoutes.some((route) => route === pathname)) {
+    return null;
+  }
+
+  return (
+    <button onClick={() => router.back()}>
+      <Image src="/back-button.svg" alt="Go back" width={25} height={25} />
+    </button>
+  );
+};
 
 export const CircularButton = ({
   imgSrc,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+import { BackButton } from "./components/client";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -7,15 +9,16 @@ export const metadata: Metadata = {
     "Campus buddy is an all-in-one solution for providing common services to college and university students.",
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" />
       </head>
       <body>
-        <main className="flex flex-col bg-gradient-to-b from-indigo-500 to-neutral-800 justify-center p-9 box-border text-white">
-          {children}
+        <main className="bg-gradient-to-b from-indigo-500 to-neutral-800 p-9 box-border text-white">
+          <BackButton />
+          <div className="h-full flex flex-col justify-center">{children}</div>
         </main>
       </body>
     </html>
